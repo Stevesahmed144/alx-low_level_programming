a6bf9e02615593ca065c989f250673262555d665
Stevesahmed144 <abdellatifwork144@gmail.com>
2023-06-15 12:55:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
 #!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
